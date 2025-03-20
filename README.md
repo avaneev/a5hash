7b3f306ffa86b6e7bda233174de4ac9c27ea3c8e
Aleksey Vaneev <D:\The Bat!\Mail>
2025-03-20 17:45:07 +0300

    Initial version 1.0 release.

--- README.md
+++ README.md
@@ -1,2 +1,91 @@
-# a5hash
-Fast, high-quality hash function for hash-tables and hash-maps (non-cryptographic, inline C/C++) + PRNG
+# A5HASH - Fast Hash Function (in C/C++) #
+
+## Introduction ##
+
+The `a5hash()` function available in the `a5hash.h` file implements a fast
+64-bit hash function, designed for hash-table, hash-map, and bloom-filter
+uses. Function's code is portable, cross-platform, scalar, zero-allocation,
+is header-only, inlineable C (C++ compatible).
+
+This function features a very high hashing throughput for small
+strings/messages (about 11 cycles/hash for 0-64-byte strings). The bulk
+throughput is, however, only moderately fast (10-15 GB/s), and that is for a
+purpose... All newest competing "fast" hash functions try to be fast both in
+ordinary keyed string hash-maps, and in large data hashing. In most cases,
+this is done for the sake of better looks in benchmarks as such hash functions
+rarely offer streamed hashing required for large data or file hashing.
+
+`a5hash` was designed to be "ultimatively" fast only for hash-tables and
+hash-maps, by utilizing "forced inlining" feature present in all modern C/C++
+compilers: this is easily achievable since in compiled binary form, `a5hash`
+is very small - 360-400 bytes, depending on compiler. Moreover, if the default
+seed (0) is used, or if a constant-size data is being hashed, this further
+reduces the code size and increases the hashing throughput.
+
+`a5hash` produces different hashes on big- and little-endian systems. This is
+a deliberate design choice, to narrow down the scope of uses to run-time
+hashing. If you need a reliable and fast hash function for files, with
+portable hashes, the [komihash](https://github.com/avaneev/komihash) is a
+great choice.
+
+In overall, `a5hash` achieves three goals: ultimate speed at run-time hashing,
+very small code size, and use of a novel mathematical construct. Compared to
+most, if not all, existing hash functions, `a5hash` does not use accumulators:
+the 128-bit result of multiplication is used directly as input on the next
+iteration. It is most definite that mathematics does not offer any simpler way
+to perform hashing and pseudo-random number generation than that.
+
+This function passes all [SMHasher](https://github.com/rurban/smhasher) and
+[SMHasher3](https://gitlab.com/fwojcik/smhasher3) tests.
+
+```c
+#include <stdio.h>
+#include "a5hash.h"
+
+int main()
+{
+    const char s1[] = "This is a test of a5hash.";
+    const char s2[] = "7 chars";
+
+    printf( "%llx\n", a5hash( s1, strlen( s1 ), 0 )); // dc668fa09c910a72
+    printf( "%llx\n", a5hash( s2, strlen( s2 ), 0 )); // 407efc4aecdf8f80
+}
+```
+
+## A5RAND ##
+
+The `a5rand()` function available in the `a5hash.h` file implements a
+simple, but reliable, self-starting, and fast (`0.50` cycles/byte) 64-bit
+pseudo-random number generator (PRNG) with `2^64` period. It is based on the
+same mathematical construct as the `a5hash` hash function, but uses
+additions of constants instead of XORs, for better statistics. `a5rand` passes
+`PractRand` tests.
+
+```c
+#include <stdio.h>
+#include "a5hash.h"
+
+int main()
+{
+    uint64_t Seed1 = 0, Seed2 = 0;
+    int i;
+
+	for( i = 0; i < 8; i++ )
+    {
+        printf( "%llx\n", a5rand( &Seed1, &Seed2 ));
+    }
+}
+```
+
+Output:
+
+```
+2492492492492491
+83958cf072b19e08
+1ae643aae6b8922e
+f463902672f2a1a0
+f7a47a8942e378b5
+778d796d5f66470f
+966ed0e1a9317374
+aea26585979bf755
+```
---
+++ a5hash.h
@@ -0,0 +1,344 @@
+/**
+ * @file a5hash.h
+ *
+ * @version 1.0
+ *
+ * @brief The inclusion file for the "a5hash" 64-bit hash function,
+ * the "a5rand" 64-bit PRNG.
+ *
+ * Description is available at https://github.com/avaneev/a5hash
+ *
+ * E-mail: aleksey.vaneev@gmail.com or info@voxengo.com
+ *
+ * LICENSE:
+ *
+ * Copyright (c) 2025 Aleksey Vaneev
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#ifndef A5HASH_INCLUDED
+#define A5HASH_INCLUDED
+
+/**
+ * @def A5HASH_U64_C( x )
+ * @brief Macro that defines a numeric value as unsigned 64-bit value.
+ * @param x Value.
+ */
+
+/**
+ * @def A5HASH_NOEX
+ * @brief Macro that defines the "noexcept" function specifier for C++
+ * environment.
+ */
+
+#if defined( __cplusplus ) && __cplusplus >= 201103L
+
+	#include <cstdint>
+	#include <cstring>
+
+	#define A5HASH_U64_C( x ) UINT64_C( x )
+	#define A5HASH_NOEX noexcept
+
+#else // __cplusplus
+
+	#include <stdint.h>
+	#include <string.h>
+
+	#define A5HASH_U64_C( x ) (uint64_t) ( x )
+	#define A5HASH_NOEX
+
+#endif // __cplusplus
+
+#define A5HASH_VER_STR "1.0" ///< A5HASH source code version string.
+
+#define A5HASH_VAL10 A5HASH_U64_C( 0xAAAAAAAAAAAAAAAA ) ///< `10` bit-pairs.
+#define A5HASH_VAL01 A5HASH_U64_C( 0x5555555555555555 ) ///< `01` bit-pairs.
+
+#if defined( __INTEL_COMPILER ) && __INTEL_COMPILER >= 1300 && \
+	!defined( _MSC_VER )
+
+	#define A5HASH_ICC_GCC ///< Macro that denotes the use of the ICC classic
+		///< compiler with GCC-style built-in functions.
+		///<
+
+#endif // ICC check
+
+/**
+ * @def A5HASH_LIKELY( x )
+ * @brief Likelihood macro that is used for manually-guided
+ * micro-optimization.
+ * @param x Expression that is likely to be evaluated to `true`.
+ */
+
+#if defined( __GNUC__ ) || defined( __clang__ ) || \
+	defined( __IBMC__ ) || defined( __IBMCPP__ ) || defined( A5HASH_ICC_GCC )
+
+	#define A5HASH_GCC_BUILTINS ///< Macro that denotes availability of
+		///< GCC-style built-in functions.
+		///<
+	#define A5HASH_LIKELY( x ) __builtin_expect( x, 1 )
+
+#else // GCC built-ins check
+
+	#define A5HASH_LIKELY( x ) ( x )
+
+#endif // GCC built-ins check
+
+#define A5HASH_INLINE static inline ///< Inline at compiler's discretion.
+
+/**
+ * @def A5HASH_INLINE_F
+ * @brief Macro to force code inlining.
+ */
+
+#if defined( i386 ) || defined( __i386 ) || defined( __i386__ ) || \
+	defined( _M_IX86 )
+
+	#define A5HASH_INLINE_F A5HASH_INLINE
+	#define A5HASH_X86 ///< Macro denotes x86 platform.
+
+#else // i386
+
+	#if defined( A5HASH_GCC_BUILTINS )
+
+		#define A5HASH_INLINE_F A5HASH_INLINE __attribute__((always_inline))
+
+	#elif defined( _MSC_VER )
+
+		#define A5HASH_INLINE_F A5HASH_INLINE __forceinline
+
+	#else // defined( _MSC_VER )
+
+		#define A5HASH_INLINE_F A5HASH_INLINE
+
+	#endif // defined( _MSC_VER )
+
+#endif // i386
+
+/**
+ * @{
+ * @brief Load unsigned value of corresponding bit-size from memory.
+ * @param p Load address.
+ */
+
+A5HASH_INLINE_F uint64_t a5hash_lu32( const void* const p ) A5HASH_NOEX
+{
+	uint32_t v;
+	memcpy( &v, p, 4 );
+
+	return( v );
+}
+
+A5HASH_INLINE_F uint64_t a5hash_lu64( const void* const p ) A5HASH_NOEX
+{
+	uint64_t v;
+	memcpy( &v, p, 8 );
+
+	return( v );
+}
+
+/** @} */
+
+#if defined( _MSC_VER )
+	#include <intrin.h>
+#endif // defined( _MSC_VER )
+
+/**
+ * @brief 64-bit by 64-bit unsigned multiplication with 128-bit result.
+ *
+ * @param u Multiplier 1.
+ * @param v Multiplier 2.
+ * @param[out] rl The lower half of the 128-bit result.
+ * @param[out] rh The higher half of the 128-bit result.
+ */
+
+A5HASH_INLINE_F void a5hash_umul128( const uint64_t u, const uint64_t v,
+	uint64_t* const rl, uint64_t* const rh ) A5HASH_NOEX
+{
+#if defined( _MSC_VER ) && ( defined( _M_ARM64 ) || defined( _M_ARM64EC ) || \
+	( defined( __INTEL_COMPILER ) && defined( _M_X64 )))
+
+	*rl = u * v;
+	*rh = __umulh( u, v );
+
+#elif defined( _MSC_VER ) && ( defined( _M_AMD64 ) || defined( _M_IA64 ))
+
+	*rl = _umul128( u, v, rh );
+
+#elif defined( __SIZEOF_INT128__ ) || \
+	( defined( A5HASH_ICC_GCC ) && defined( __x86_64__ ))
+
+	const __uint128_t r = (__uint128_t) u * v;
+
+	*rl = (uint64_t) r;
+	*rh = (uint64_t) ( r >> 64 );
+
+#elif ( defined( __IBMC__ ) || defined( __IBMCPP__ )) && defined( __LP64__ )
+
+	*rl = u * v;
+	*rh = __mulhdu( u, v );
+
+#else // defined( __IBMC__ )
+
+	// _umul128() code for 32-bit systems, adapted from Hacker's Delight,
+	// Henry S. Warren, Jr.
+
+	*rl = u * v;
+
+	const uint32_t u0 = (uint32_t) u;
+	const uint32_t v0 = (uint32_t) v;
+	const uint64_t w0 = (uint64_t) u0 * v0;
+	const uint32_t u1 = (uint32_t) ( u >> 32 );
+	const uint32_t v1 = (uint32_t) ( v >> 32 );
+	const uint64_t t = (uint64_t) u1 * v0 + (uint32_t) ( w0 >> 32 );
+	const uint64_t w1 = (uint64_t) u0 * v1 + (uint32_t) t;
+
+	*rh = (uint64_t) u1 * v1 + (uint32_t) ( w1 >> 32 ) +
+		(uint32_t) ( t >> 32 );
+
+#endif // defined( __IBMC__ )
+}
+
+/**
+ * @brief A5HASH 64-bit hash function.
+ *
+ * Produces and returns a 64-bit hash value of the specified message, string,
+ * or binary data block. Designed for 64-bit hash-table and hash-map uses.
+ *
+ * @param Msg0 The message to produce a hash from. The alignment of this
+ * pointer is unimportant. It is valid to pass 0 when `MsgLen` equals 0.
+ * @param MsgLen Message's length, in bytes, can be zero.
+ * @param UseSeed Optional value, to use instead of the default seed (0). This
+ * value can have any statistical quality.
+ * @return 64-bit hash of the input data.
+ */
+
+A5HASH_INLINE_F uint64_t a5hash( const void* const Msg0, size_t MsgLen,
+	const uint64_t UseSeed ) A5HASH_NOEX
+{
+	const unsigned char* Msg = (const unsigned char*) Msg0;
+
+	// The seeds are initialized to mantissa bits of PI.
+
+	uint64_t Seed1 = A5HASH_U64_C( 0x243F6A8885A308D3 ) ^ MsgLen;
+	uint64_t Seed2 = A5HASH_U64_C( 0x452821E638D01377 ) ^ MsgLen;
+
+	a5hash_umul128( Seed1 ^ ( UseSeed & A5HASH_VAL01 ),
+		Seed2 ^ ( UseSeed & A5HASH_VAL10 ), &Seed1, &Seed2 );
+
+	uint64_t a, b;
+
+	if( A5HASH_LIKELY( MsgLen < 17 ))
+	{
+		if( A5HASH_LIKELY( MsgLen > 3 ))
+		{
+			const unsigned char* const Msg4 = Msg + MsgLen - 4;
+			a = a5hash_lu32( Msg ) << 32 | a5hash_lu32( Msg4 );
+
+			const size_t mo = MsgLen >> 3;
+			b = a5hash_lu32( Msg + mo * 4 ) << 32 |
+				a5hash_lu32( Msg4 - mo * 4 );
+		}
+		else
+		if( MsgLen != 0 )
+		{
+			a = Msg[ 0 ];
+			b = 0;
+
+			if( MsgLen > 1 )
+			{
+				a |= Msg[ 1 ] << 8;
+
+				if( MsgLen > 2 )
+				{
+					a |= Msg[ 2 ] << 16;
+				}
+			}
+		}
+		else
+		{
+			a = 0;
+			b = 0;
+		}
+	}
+	else
+	{
+		while( A5HASH_LIKELY( MsgLen > 16 ))
+		{
+			a5hash_umul128( Seed1 ^ a5hash_lu64( Msg ),
+				Seed2 ^ a5hash_lu64( Msg + 8 ), &Seed1, &Seed2 );
+
+			Seed1 ^= A5HASH_VAL01;
+			Seed2 ^= A5HASH_VAL10;
+
+			MsgLen -= 16;
+			Msg += 16;
+
+		}
+
+		a = a5hash_lu64( Msg + MsgLen - 16 );
+		b = a5hash_lu64( Msg + MsgLen - 8 );
+	}
+
+	a5hash_umul128( Seed1 ^ a, Seed2 ^ b, &Seed1, &Seed2 );
+	Seed1 ^= A5HASH_VAL01;
+	Seed2 ^= A5HASH_VAL10;
+
+	a5hash_umul128( Seed1, Seed2, &Seed1, &Seed2 );
+
+	return( Seed1 ^ Seed2 );
+}
+
+/**
+ * @brief A5RAND 64-bit pseudo-random number generator.
+ *
+ * Simple, reliable, self-starting yet efficient PRNG, with 2^64 period.
+ * 0.50 cycles/byte performance. Self-starts in 4 iterations, which is a
+ * suggested "warm up" before using its output, if seeds were initialized with
+ * arbitrary values.
+ *
+ * The `Seed1` and `Seed2` variables can be independently initialized with two
+ * high-quality uniformly-random values (e.g., from operating system's
+ * entropy, or a hash function's outputs). Such initialization reduces the
+ * number of "warm up" iterations - that way the PRNG output will be valid
+ * from the let go.
+ *
+ * @param[in,out] Seed1 Seed value 1. Can be initialized to any value
+ * (even 0). Should not be used as PRNG value.
+ * @param[in,out] Seed2 Seed value 2. In the simplest case, can be initialized
+ * to the same value as `Seed1`. Should not be used as PRNG value.
+ * @return The next uniformly-random 64-bit value.
+ */
+
+A5HASH_INLINE_F uint64_t a5rand( uint64_t* const Seed1,
+	uint64_t* const Seed2 ) A5HASH_NOEX
+{
+	uint64_t s1 = *Seed1;
+	uint64_t s2 = *Seed2;
+
+	a5hash_umul128( s1 + A5HASH_VAL01, s2 + A5HASH_VAL10, &s1, &s2 );
+
+	*Seed1 = s1;
+	*Seed2 = s2;
+
+	return( s1 ^ s2 );
+}
+
+#endif // A5HASH_INCLUDED
